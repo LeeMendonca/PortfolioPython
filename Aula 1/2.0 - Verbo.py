f64e164b758cdbe51d9ed770d5fad75daebabc96
Letícia F Mendonça <leticiamendonca2110@gmail.com>
2024-12-10 01:37:50 -0300

    Delete Aula 1 directory

--- Aula 1/2.0 - Verbo.py
+++
@@ -1,9 +0,0 @@
-'''
-Exercício 2 - Verbo
-
-Escreva um algoritmo que solicite um verbo e então, deve ser mostrado na tela
-"Você é capaz de " concatenando o verbo que foi escrito,
-e seguido do ponto de interrogação.
-'''
-verbo = input('Digite um verbo: ')
-print('Você é capaz de', verbo, '?')
