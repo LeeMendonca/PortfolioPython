14bfb4ea51bc8bc8b1f48ec287bc1a0b3faaaad3
Letícia F Mendonça <leticiamendonca2110@gmail.com>
2024-12-10 01:39:04 -0300

    Add files via upload

---
+++ Aula 1 - Input e Print/2.0 - Verbo.py
@@ -0,0 +1,9 @@
+'''
+Exercício 2 - Verbo
+
+Escreva um algoritmo que solicite um verbo e então, deve ser mostrado na tela
+"Você é capaz de " concatenando o verbo que foi escrito,
+e seguido do ponto de interrogação.
+'''
+verbo = input('Digite um verbo: ')
+print('Você é capaz de', verbo, '?')
