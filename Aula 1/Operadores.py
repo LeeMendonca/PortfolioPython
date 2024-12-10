e51242e42c91132eec646f46a9b43f3184975c22
Letícia F Mendonça <leticiamendonca2110@gmail.com>
2024-12-10 01:31:43 -0300

    Add files via upload

---
+++ Aula 1/Operadores.py
@@ -0,0 +1,18 @@
+''' OPERADORES '''
+
+# Operadores de Comparação:
+'''
+a  ==  b : igual
+a  !=  b : diferente
+a  <   b : menor
+a  >   b : maior
+a  <=  b : menor ou igual
+a  >=  b : maior ou igual
+'''
+# Operadores com Portas Lógicas:
+'''
+a   is   b : são idênticos
+a is not b : não são idênticos
+a   in   b : a é membro de b
+a not in b : a não é membro de b
+'''
