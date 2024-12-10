1cad5060907771820cd6e85700e6a90962e660c9
Letícia F Mendonça <leticiamendonca2110@gmail.com>
2024-12-10 01:33:36 -0300

    Add files via upload

---
+++ Aula 0/Constantes (true, false, none).py
@@ -0,0 +1,12 @@
+'''
+CONSTANTES
+Embutidas no Python: True, False e None.
+True e False são valores booleanos que representam, respectivamente, verdadeiro e falso. A função bool() retorna o valor booleano de um argumento.
+'''
+print(bool(1 == '1'))
+'''
+A função resulta False em strings vazias, quando um número é 0 ou quando é None.
+O None é um valor do tipo NoneType e é usado para representar a abstenção de um valor.
+Em outras linguagens, como Java e C#, é comum utilizar a palavra Null para abstenção de valor.
+'''
+print(type(None))
