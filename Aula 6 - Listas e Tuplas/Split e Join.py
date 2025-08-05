caaf675d8924768ea08c38a930121cba13b84239
Letícia F Mendonça <leticiamendonca2110@gmail.com>
2025-08-05 16:01:31 -0300

    Delete Aula 6 - Listas e Tuplas directory

--- Aula 6 - Listas e Tuplas/Split e Join.py
+++
@@ -1,8 +0,0 @@
-''' Split e Join '''
-
-frase3 = "essa é uma frase com muitas palavras"
-lista_palavras=frase3.split()
-print(lista_palavras)
-
-nova_frase = " ".join(lista_palavras)
-print(nova_frase)
